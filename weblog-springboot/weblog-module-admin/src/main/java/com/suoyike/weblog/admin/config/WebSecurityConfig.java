df35ed838b8cdba762df2a2ec599c9ed375d8fed
suoyike66 <3590646448@qq.com>
2025-12-30 17:54:01 +0800

    Spring Security 整合 JWT ：实现接口鉴权

--- weblog-springboot/weblog-module-admin/src/main/java/com/suoyike/weblog/admin/config/WebSecurityConfig.java
+++ weblog-springboot/weblog-module-admin/src/main/java/com/suoyike/weblog/admin/config/WebSecurityConfig.java
@@ -1,12 +1,17 @@
 package com.suoyike.weblog.admin.config;
 
 import com.suoyike.weblog.jwt.config.JwtAuthenticationSecurityConfig;
+import com.suoyike.weblog.jwt.filter.TokenAuthenticationFilter;
+import com.suoyike.weblog.jwt.handler.RestAccessDeniedHandler;
+import com.suoyike.weblog.jwt.handler.RestAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author: 蓑衣客
@@ -20,6 +25,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JwtAuthenticationSecurityConfig jwtAuthenticationSecurityConfig;
+    @Autowired
+    private RestAuthenticationEntryPoint authEntryPoint;
+    @Autowired
+    private RestAccessDeniedHandler deniedHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,7 +40,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/admin/**").authenticated() // 认证所有以 /admin 为前缀的 URL 资源
                 .anyRequest().permitAll() // 其他都需要放行，无需认证
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 前后端分离，无需创建会话
+                .httpBasic().authenticationEntryPoint(authEntryPoint) // 处理用户未登录访问受保护的资源的情况
+                .and()
+                .exceptionHandling().accessDeniedHandler(deniedHandler) // 处理登录成功后访问受保护的资源，但是权限不够的情况
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 前后端分离，无需创建会话
+                .and()
+                .addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class) // 将 Token 校验过滤器添加到用户认证过滤器之前
+        ;
+    }
+
+    /**
+     * Token 校验过滤器
+     * @return
+     */
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter();
     }
 
 }
+
--- weblog-springboot/weblog-module-jwt/src/main/java/com/suoyike/weblog/jwt/utils/JwtTokenHelper.java
+++ weblog-springboot/weblog-module-jwt/src/main/java/com/suoyike/weblog/jwt/utils/JwtTokenHelper.java
@@ -29,6 +29,12 @@ public class JwtTokenHelper implements InitializingBean {
      */
     @Value("${jwt.issuer}")
     private String issuer;
+
+    /**
+     * Token 失效时间（分钟）
+     */
+    @Value("${jwt.tokenExpireTime}")
+    private Long tokenExpireTime;
     /**
      * 秘钥
      */
@@ -68,8 +74,8 @@ public class JwtTokenHelper implements InitializingBean {
      */
     public String generateToken(String username) {
         LocalDateTime now = LocalDateTime.now();
-        // Token 一个小时后失效
-        LocalDateTime expireTime = now.plusHours(1);
+        // 设置 Token 失效时间
+        LocalDateTime expireTime = now.plusMinutes(tokenExpireTime);
 
         return Jwts.builder().setSubject(username)
                 .setIssuer(issuer)
@@ -92,6 +98,31 @@ public class JwtTokenHelper implements InitializingBean {
         } catch (ExpiredJwtException e) {
             throw new CredentialsExpiredException("Token 失效", e);
         }
+    }
+
+    /**
+     * 校验 Token 是否可用
+     * @param token
+     * @return
+     */
+    public void validateToken(String token) {
+        jwtParser.parseClaimsJws(token);
+    }
+
+    /**
+     * 解析 Token 获取用户名
+     * @param token
+     * @return
+     */
+    public String getUsernameByToken(String token) {
+        try {
+            Claims claims = jwtParser.parseClaimsJws(token).getBody();
+            String username = claims.getSubject();
+            return username;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     /**
