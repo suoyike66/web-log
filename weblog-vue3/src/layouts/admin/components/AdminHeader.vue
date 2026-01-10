ba0bce02787a1749a3b1f7c29ee08f91f5d5c939
suoyike66 <3590646448@qq.com>
2026-01-10 17:12:49 +0800

    feat(admin): 实现标签页导航功能并优化头部样式
    - 新增标签页导航组件，支持动态添加、切换和存储标签页状态
    - 优化AdminHeader组件样式，调整边框颜色并添加刷新功能
    - 添加全屏切换图标状态显示
    - 创建tag-list.js composable用于管理标签页数据

--- weblog-vue3/src/layouts/admin/components/AdminHeader.vue
+++ weblog-vue3/src/layouts/admin/components/AdminHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 后台的头部 -->
 	<!-- 通过 flex 指定水平布局 -->
-    <div class="bg-white h-[64px] flex pr-4 border-b border-slate-200">
+    <div class="bg-white h-[64px] flex pr-4 border-b border-slate-100">
 		<!-- 左边栏收缩、展开 -->
         <div class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 hover:bg-gray-200" @click="handleMenuWidth">
             <el-icon>
@@ -11,9 +11,10 @@
         </div>
         <!-- 右边容器，通过 ml-auto 让其在父容器的右边 -->
         <div class="ml-auto flex items-center">
-            <!-- 刷新按钮 -->
+			<!-- 点击刷新页面 -->
             <el-tooltip class="box-item" effect="dark" content="刷新" placement="bottom">
-                <div class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 mr-2 hover:bg-gray-200">
+                <div class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 hover:bg-gray-200"
+                    @click="handleRefresh">
                     <el-icon>
                         <Refresh />
                     </el-icon>
@@ -33,13 +34,14 @@
             <el-tooltip class="box-item" effect="dark" content="全屏" placement="bottom">
                 <div class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 mr-2 hover:bg-gray-200" @click="toggle">
                     <el-icon>
-                        <FullScreen/>
+                        <FullScreen v-if="!isFullscreen"/>
+                        <Aim v-else/>
                     </el-icon>
                 </div>
             </el-tooltip>
 
             <!-- 登录用户头像 -->
-            <el-dropdown>
+           <el-dropdown class="flex items-center justify-center">
                 <span class="el-dropdown-link flex items-center justify-center text-gray-700 text-xs">
                     <!-- 头像 Avatar -->
                     <el-avatar class="mr-2" :size="25" src="https://img.quanxiaoha.com/quanxiaoha/f97361c0429d4bb1bc276ab835843065.jpg" />
@@ -72,4 +74,6 @@ import { useFullscreen } from '@vueuse/core'
         // 动态设置菜单的宽度大小
         menuStore.handleMenuWidth()
     }
+    // 刷新页面
+    const handleRefresh = () => location.reload()
 </script>
