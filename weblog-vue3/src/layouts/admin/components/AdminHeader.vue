0a1211c2020bbbffa0d3a00343a651f4b2f44e01
suoyike66 <3590646448@qq.com>
2026-01-10 16:00:02 +0800

    feat(admin): 添加后台管理页面和布局组件
    添加文章管理、分类管理、标签管理和博客设置页面
    实现后台布局组件包括菜单栏和顶部导航栏
    配置路由以支持新的管理页面结构

---
+++ weblog-vue3/src/layouts/admin/components/AdminHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- 后台的头部 -->
+	<!-- 通过 flex 指定水平布局 -->
+    <div class="bg-white h-[64px] flex pr-4 border-b border-slate-200">
+        <!-- 左边栏收缩、展开 -->
+      <div class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 hover:bg-gray-200">
+        <el-icon>
+            <Fold />
+        </el-icon>
+      </div>
+        <!-- 右边容器，通过 ml-auto 让其在父容器的右边 -->
+        <div class="ml-auto flex items-center">
+            <!-- 刷新按钮 -->
+            <el-tooltip class="box-item" effect="dark" content="刷新" placement="bottom">
+                <div class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 mr-2 hover:bg-gray-200">
+                    <el-icon>
+                        <Refresh />
+                    </el-icon>
+                </div>
+            </el-tooltip>
+
+            <!-- 布局切换按钮 -->
+            <el-tooltip class="box-item" effect="dark" content="布局" placement="bottom">
+                <div class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 mr-2 hover:bg-gray-200">
+                    <el-icon>
+                        <Grid />
+                    </el-icon>
+                </div>
+            </el-tooltip>
+
+            <!-- 点击全屏展示 -->
+            <el-tooltip class="box-item" effect="dark" content="全屏" placement="bottom">
+                <div class="w-[42px] h-[64px] cursor-pointer flex items-center justify-center text-gray-700 mr-2 hover:bg-gray-200">
+                    <el-icon>
+                        <FullScreen />
+                    </el-icon>
+                </div>
+            </el-tooltip>
+
+            <!-- 登录用户头像 -->
+            <el-dropdown>
+                <span class="el-dropdown-link flex items-center justify-center text-gray-700 text-xs">
+                    <!-- 头像 Avatar -->
+                    <el-avatar class="mr-2" :size="25" src="https://img.quanxiaoha.com/quanxiaoha/f97361c0429d4bb1bc276ab835843065.jpg" />
+                    test
+                    <el-icon class="el-icon--right">
+                        <arrow-down />
+                    </el-icon>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>修改密码</el-dropdown-item>
+                        <el-dropdown-item>退出登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </div>
+    </div>
+</template>
+
