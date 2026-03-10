fc899fdd43f585dee8345e841bdf6c8d09bf557c
suoyike66 <3590646448@qq.com>
2026-03-10 22:45:59 +0800

    feat(search): 实现 Lucene 全文检索功能
    添加 Lucene 相关配置和工具类，包括索引创建、文档处理和初始化逻辑
    在配置文件中添加 Lucene 索引目录配置
    新增 commons-io 依赖用于文件操作

--- weblog-springboot/weblog-module-common/src/main/java/com/suoyike/weblog/common/constant/Constants.java
+++ weblog-springboot/weblog-module-common/src/main/java/com/suoyike/weblog/common/constant/Constants.java
@@ -2,9 +2,20 @@ package com.suoyike.weblog.common.constant;
 
 import java.time.format.DateTimeFormatter;
 
+/**
+ * @author: 蓑衣客
+ * @url: www.suoyikehan.com
+ * @date: 2026/03/10 22:42
+ * @description: 全局静态变量
+ **/
 public interface Constants {
     /**
      * 月-日 格式
      */
     DateTimeFormatter MONTH_DAY_FORMATTER = DateTimeFormatter.ofPattern("MM-dd");
+
+    /**
+     * 年-月-日 小时-分钟-秒
+     */
+    DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 }
